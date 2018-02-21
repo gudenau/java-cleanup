d5b7ad4d3b16e28935ae5afe8235884e0db28e55
gudenau <gudenaua@gmail.com>
2018-02-20 23:07:17 -0600

    Initial commit

--- src/main/java/net/gudenau/lib/cleanup/Cleaner.java
+++ src/main/java/net/gudenau/lib/cleanup/Cleaner.java
@@ -2,6 +2,7 @@ package net.gudenau.lib.cleanup;
 
 import java.lang.ref.PhantomReference;
 import java.lang.ref.ReferenceQueue;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import net.gudenau.lib.annotation.NonNull;
@@ -14,7 +15,7 @@ public class Cleaner extends PhantomReference<Object>{
     private static final ReferenceQueue<Object> referenceQueue = new ReferenceQueue<>();
     // Because it won't work without
     @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
-    private static final List<Cleaner> cleaners = new LinkedList<>();
+    private static final List<Cleaner> cleaners = new ArrayList<>();
     private static final Thread cleanerThread;
     private static final Object $LOCK$ = new Object[0];
     
@@ -41,23 +42,49 @@ public class Cleaner extends PhantomReference<Object>{
             }catch(InterruptedException ignored){
                 continue;
             }
-            cleaner.callback.run();
+            cleaner.clean();
             synchronized($LOCK$){
                 cleaners.remove(cleaner);
             }
         }
     }
     
-    public static void addCleaner(@NonNull Object object, @NonNull Runnable callback){
+    /**
+     * Creates a new {@link net.gudenau.lib.cleanup.Cleaner Cleaner}.
+     *
+     * Make sure there are no references to the object inside the callback!
+     *
+     * @param object The object to watch
+     * @param callback The callback to clean the object
+     * */
+    public static Cleaner addCleaner(@NonNull Object object, @NonNull Runnable callback){
         synchronized($LOCK$){
-            cleaners.add(new Cleaner(object, callback));
+            Cleaner cleaner = new Cleaner(object, callback);
+            cleaners.add(cleaner);
+            return cleaner;
         }
     }
     
     private final Runnable callback;
+    private volatile boolean clean = false;
     
     private Cleaner(Object reference, Runnable callback){
         super(reference, referenceQueue);
         this.callback = callback;
+    }
+    
+    /**
+     * Invoke the cleaner callback, if it has yet to be invoked.
+     * */
+    public void clean(){
+        synchronized(this){
+            if(!clean){
+                clean = true;
+                callback.run();
+                synchronized($LOCK$){
+                    cleaners.remove(this);
+                }
+            }
+        }
     }
 }
